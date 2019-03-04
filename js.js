6d9b596d206d7e80a1d5c0e0586a51472d11fa6e
mrbacco <abaccolini@gmail.com>
2019-03-04 20:57:11 +0000

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+var Messages = new Array();
+Messages[0] = "This is message 1";
+Messages[1] = "This is message 2";
+Messages[2] = "This is message 3";
+Messages[3] = "This is message 4";
+
+function scroll_messages()
+{
+  for (var i = 0; i < Messages.length; i++)
+        document.write(Message[i]);
+}
